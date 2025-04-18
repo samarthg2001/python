f57bc2ca4ca164f23efb61420e2d707a35958607
samarth <samganamote@gmail.com>
2025-04-18 23:37:55 +0530

    second comand

--- one/python.py
+++ one/python.py
@@ -1,1 +1,20 @@
+#this is first python code 
 print("hello world")
+
+full_name="samarth "
+age=25 
+gpa=2.5
+# True or Flase first letter should be capital 
+isStudent=False
+
+#f means format , we have to mention it before inserting the variable in print statemet 
+#to insert variable we can insert by using { variable name} 
+print(f"hello {full_name}")
+print(f"your age is  {age}")
+print(f"my gpa is  {gpa}")
+
+
+if isStudent:
+    print(f"{full_name}")
+else :
+    print("your are not student ")
