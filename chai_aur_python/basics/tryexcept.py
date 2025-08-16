9a8d0db0d48d9b5532e1036462bfca2ebf09a05b
samarth <samganamote@gmail.com>
2025-08-16 20:06:37 +0530

    16-08-2025

---
+++ chai_aur_python/basics/tryexcept.py
@@ -0,0 +1,29 @@
+try:
+    a=10/0
+except :
+    print("hello world")
+    
+    
+def fun(*a):
+    for i in a:
+        print(i)
+
+my_list=[10,20,30,4,5,6,8,]
+
+my_value=("afds","fasfas","fdasfd" ,"")
+
+fun(my_list)
+fun(my_value)
+
+fun(9,10,2,3,5,60)
+
+def sample(a,b,c,d):
+    print(a,b,c,d)
+def sample1(*t):
+    for i in t:
+        print(i)
+
+sample1(*my_list)
+sample1(*my_value)
+# sample(*my_list) TypeError: sample() takes 4 positional arguments but 7 were given
+sample(*my_value)
