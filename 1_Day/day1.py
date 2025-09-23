48a1b6594631f1da2e57ab5abcb564f3b231c339
samarth <samganamote@gmail.com>
2025-09-23 20:53:11 +0530

    Day1

---
+++ 1_Day/day1.py
@@ -0,0 +1,13 @@
+# different way to create a list 
+empty_list=[]
+empty_list2=list()
+
+
+#list with element
+Alphabets=['a','b','c','d']
+number=[1,23,5,6,80]
+mixed=['d1',5,9,'d',1.5]
+
+# using list for range
+from_range=list(range(1,10))
+from_string=list("hello")
