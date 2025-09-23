d0af6f6abd421afca6a723179e020d3bc246d985
samarth <samganamote@gmail.com>
2025-09-23 20:53:54 +0530

    Day1

--- 1_Day/day1.py
+++ 1_Day/day1.py
@@ -7,7 +7,20 @@ empty_list2=list()
 Alphabets=['a','b','c','d']
 number=[1,23,5,6,80]
 mixed=['d1',5,9,'d',1.5]
-
 # using list for range
 from_range=list(range(1,10))
 from_string=list("hello")
+
+print(f"fruits: {Alphabets}")
+print(f"numbers: {number}")
+print(f"from_range: {from_range}")
+print(f"from_range: {from_string}")
+
+# update or midify 
+
+number[5]=156
+mixed[0:2]=["sfdd",460]
+
+
+
+
