7bd2666a05752d7db0fc4b89362c7205bee703e5
samarth <samganamote@gmail.com>
2025-09-24 21:01:23 +0530

    day2

--- 1_Day/day2.py
+++ 1_Day/day2.py
@@ -1,4 +1,4 @@
-search_list=[12,3,56,8,2,6,82]
+search_list=[12,3,56,8,2,6,82,2,65,56,2]
 
 print("liner search")
 search_element=int(input("Enter the value that need to find in list "))
@@ -16,3 +16,15 @@ if not flag :
     print("Value is not found in list")
 
 print("program end")
+
+
+def count_element(listvalue, element):
+    count=0
+    for i in listvalue:
+        if i == element:
+            count=count+1
+    return count;
+
+
+repated_number=count_element(search_list,2)
+print(f"repated numbers {repated_number}")
